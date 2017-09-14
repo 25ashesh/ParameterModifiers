5032d090638e2e9e03ba5e6596dc5f4f4cee0ba2
Ashesh <subedi.ashesh@gmail.com>
2017-09-14 13:28:10 -0500

    Update ParameterModifiers.cs

--- ParameterModifiers.cs
+++ ParameterModifiers.cs
@@ -1,1 +1,14 @@
 
+class ParamterModifiers
+    {
+        static void Add(int x, int y, out int result)
+        {
+            result = x + y;
+        }
+        public static void Main(string[] args)
+        {
+            int ans;
+            Add(90, 90, out ans);
+            Console.WriteLine("The result is: "+ans);
+        }
+    }
