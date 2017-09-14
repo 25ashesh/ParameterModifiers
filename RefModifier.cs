82d87155a62058ed1e08836b7391f6da0a6200d4
Ashesh <subedi.ashesh@gmail.com>
2017-09-14 14:23:14 -0500

    Update RefModifier.cs

--- RefModifier.cs
+++ RefModifier.cs
@@ -1,1 +1,20 @@
 
+class RefModifier
+    {
+        public static void Swapper(ref string fname, ref string lname)
+        {
+            string temp = fname;
+            fname = lname;
+            lname = temp;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("First Name: ");
+            string str1 = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string str2 = Console.ReadLine();
+            Console.WriteLine("Originally : {0} {1}", str1 , str2);
+            Swapper(ref str1, ref str2);
+            Console.WriteLine("After swapping: {0} {1}", str1, str2);
+
+        }
